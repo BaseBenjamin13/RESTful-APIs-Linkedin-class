5570dc47bd87a00948101ee9cbdbc5655adcac7b
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-12-23 02:07:15 -0500

    Schema setup and mongo uri

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import routes from './src/routes/crmRoutes';
+require('dotenv').config()
 
 const app = express();
 const PORT = 3000;
 
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:CRMdb', {
+mongoose.connect(`mongodb+srv://benjamin_m:${process.env.DB_PASS}@cluster0.9xnz1.mongodb.net/CRM?retryWrites=true&w=majority`, {
     useNewUrlParser: true
 });
 
