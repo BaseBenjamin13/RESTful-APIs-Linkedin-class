852e4fa96a9aea0cc20ae7b8ab14bf1d74979860
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-01-05 21:57:12 -0500

    made contact controller

---
+++ src/controllers/crmController.js
@@ -0,0 +1,19 @@
+
+import res from "express/lib/response";
+import mongoose from "mongoose";
+import { ContactSchema } from '../models/crmModel';
+
+
+const Contact = mongoose.model('Contact', ContactSchema);
+
+export const addNewContact = (req, res) => {
+    let newContact = new Contact(req.body)
+
+    newContact.save((err, contact) => {
+        if(err) {
+            res.send(err);
+        }
+        res.json(contact);
+    });
+};
+
